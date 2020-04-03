a0a3832fd14f45d1621781792efb9d29a419aa4f
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2020-04-03 15:13:54 +0200

    Level 1

--- src/cli.py
+++ src/cli.py
@@ -1,15 +1,24 @@
 import os
 from pprint import pprint
+from collections import namedtuple
 
 from contest import solve
 
+FlightEntry = namedtuple("FlightEntry", "timestamp lat long altitude")
+
 
 def load(data):
-    return data
+    n = int(data[0])
+    flights = [FlightEntry(*map(float, line.split(","))) for line in data[1:]]
+
+    return {
+        "n": n,
+        "flights": flights
+    }
 
 
 if __name__ == "__main__":
-    level, quests = 1, 4
+    level, quests = 1, 5
     for q in range(1, quests + 1):
         input_file = r'..\data\level{0}\level{0}_{1}.in'.format(level, q)
         output_file = os.path.splitext(input_file)[0] + ".out"
--- src/contest.py
+++ src/contest.py
@@ -1,2 +1,15 @@
 def solve(data):
-    return "Result"
+    flights = data["flights"]
+
+    minTimestamp = min([f.timestamp for f in flights])
+    maxTimestamp = max([f.timestamp for f in flights])
+    minLat = min([f.lat for f in flights])
+    maxLat = max([f.lat for f in flights])
+    minLong = min([f.long for f in flights])
+    maxLong = max([f.long for f in flights])
+    maxAltitude = max([f.altitude for f in flights])
+
+    return f"{minTimestamp:.0f} {maxTimestamp:.0f}\n" + \
+           f"{minLat} {maxLat}\n" + \
+           f"{minLong} {maxLong}\n" + \
+           f"{maxAltitude}\n"
